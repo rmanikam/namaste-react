073d1959791bdbe6290bede4f5df61cbaa629dd3
Raman Sahi <sahiraman7@gmail.com>
2023-10-21 13:15:04 +0530

    Assignment 2

--- App.js
+++ App.js
@@ -1,4 +1,5 @@
-
+import React from "react"; // this means we are getting react from our node modules
+import ReactDOM from "react-dom/client"; // this means we are getting react from our node modules
 //  <div id ="parent">
 
 
@@ -59,6 +60,14 @@
 // root.render(parent);
 
 // const heading = React.createElement("h1", {id: "heading", xyz: "abc"}, "Hello World from React")
+
+// React.createElement will give me an plain JS object of type h1
+
+// whenever i reloading my page it takes some time for react to load into the page
+// go to network, go to all, react is loading the script and it takes some time to (disable the cache though and see 200 ones only) come 
+// our browser loads the website and then it starts reading the things written inside index.html
+
+
 // i need to render my heading inside root, this
 // div id. React needs to have a root where 
 // it can do all the DOM stuff, all DOM manipulation
@@ -86,10 +95,19 @@
 //     </div>
 // </div> 
 
+// const heading = React.createElement("h1", style: {}, "Hello World from React")
 
+// const heading = React.createElement("h1", {id: "heading", className: "abc"}, "Hello World from React")
 
-const parent = React.createElement("div", {id:"parent"}, [React.createElement("div", {id:"child1"}, [React.createElement("h1", {}, "I'm h1 tag"), React.createElement("h2", {}, "I'm h1 tag")]), React.createElement("div", {id:"child2"}, [React.createElement("h1", {}, "I'm h1 tag"), React.createElement("h2", {}, "I'm h1 tag")])])
+// const heading = React.createElement("h1", {id: "heading", xyz: "abc"}, "Hello World from React")
+
+// console.log(heading);
+
+ const parent = React.createElement("div", {id:"parent"}, [React.createElement("div", {id:"child1"}, [React.createElement("h1", {}, "I'm h1 tag"), React.createElement("h2", {}, "I'm h1 tag")]), React.createElement("div", {id:"child2"}, [React.createElement("h1", {}, "I'm h1 tag"), React.createElement("h2", {}, "I'm h1 tag")])])
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+
+// here i am passing a react element (parent in this case) inside root.
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-
 root.render(parent);
